98974de735682ddf0f8527a5b678282230f69375
Admin <hubtth07@gmail.com>
2020-08-05 15:11:23 +0700

    Initial commit

---
+++ app/src/main/java/kot/android/spinn/MainActivity.java
@@ -0,0 +1,52 @@
+package kot.android.spinn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    List<String> numbers = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setPrompt("Hello");
+        numbers.add(0, "Select numbers");
+        numbers.add("1");
+        numbers.add("2");
+        numbers.add("3");
+        numbers.add("5");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,numbers);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(adapterView.getItemAtPosition(i).equals("Select numbers")){
+
+                }
+                else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinner.setAdapter(adapter);
+    }
+}
